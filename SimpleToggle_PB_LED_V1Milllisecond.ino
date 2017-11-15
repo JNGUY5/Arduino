425bfddb543b74e759340dcb8475e8b36678deae
JNGUY5 <31697759+JNGUY5@users.noreply.github.com>
2017-11-15 11:51:39 -0500

    This is the code for adding Using miliseconds instead of delay
    In this code I use milliseconds instead of delay, for the same purpose. I can now fix the bounce issue without using delay, so that it doesn't shut down the system if i ever work in groups.

---
+++ SimpleToggle_PB_LED_V1Milllisecond.ino
@@ -0,0 +1,42 @@
+    /* SimpleToggle_PB_LED */
+long interval = 150;
+long Beforemillis = 0;
+const byte LEDpin = 3;
+const byte SWITCHpin = 2;
+
+byte LEDstate = HIGH;
+byte SWITCHstate;
+byte lastSWITCHstate;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LEDpin, OUTPUT);
+pinMode(SWITCHpin, INPUT);
+digitalWrite(SWITCHpin, HIGH);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+  SWITCHstate = digitalRead(SWITCHpin);
+
+  if ((lastSWITCHstate == HIGH) && (SWITCHstate == LOW))
+  {
+
+   unsigned long currentMillis = millis();
+ 
+    if(currentMillis - Beforemillis > interval){
+    Beforemillis = currentMillis;   
+
+      LEDstate = !LEDstate;
+      digitalWrite(LEDpin, LEDstate);
+    }
+    
+   
+    
+    
+    
+}
+lastSWITCHstate = SWITCHstate;
+}
