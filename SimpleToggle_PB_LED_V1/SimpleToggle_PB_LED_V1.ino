7fe1f1965a9c3dd57c0e8b8f4c92df777b800253
JNGUY5 <31697759+JNGUY5@users.noreply.github.com>
2017-11-15 11:00:46 -0500

    Adding 2 files of the arduino code.
    this is the basic code that is used for button click of arduino.  the Simple_PB_LED_V1/Simple_PB_LED_V1.ino is not working, you press for light on, when let go it goes away. The second one should be better.

---
+++ SimpleToggle_PB_LED_V1/SimpleToggle_PB_LED_V1.ino
@@ -0,0 +1,29 @@
+    /* SimpleToggle_PB_LED */
+
+const byte LEDpin = 3;
+const byte SWITCHpin = 2;
+
+byte LEDstate = HIGH;
+byte SWITCHstate;
+byte lastSWITCHstate;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LEDpin, OUTPUT);
+pinMode(SWITCHpin, INPUT);
+digitalWrite(SWITCHpin, HIGH);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+SWITCHstate = digitalRead(SWITCHpin);
+if ((lastSWITCHstate == HIGH) && (SWITCHstate == LOW))
+{
+    LEDstate = !LEDstate;
+    digitalWrite(LEDpin, LEDstate);
+    
+}
+lastSWITCHstate = SWITCHstate;
+}
---
+++ Simple_PB_LED_V1/Simple_PB_LED_V1.ino
@@ -0,0 +1,20 @@
+        /*Simple_PB_LED_V1   */
+
+const byte LEDpin = 3;
+const byte SWITCHpin = 2;
+
+byte LEDstate = LOW;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LEDpin, OUTPUT);
+pinMode(SWITCHpin, INPUT);
+digitalWrite(SWITCHpin, HIGH);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+LEDstate = digitalRead(SWITCHpin);
+digitalWrite(LEDpin, LEDstate);
+}
