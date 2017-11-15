6efe54654e0094e26f948902563068a9acf97cd2
JNGUY5 <31697759+JNGUY5@users.noreply.github.com>
2017-11-15 11:06:31 -0500

    This is the arduino file with the fix.

---
+++ SimpleToggle_PB_LED_V1LightFix.ino
@@ -0,0 +1,31 @@
+    /* SimpleToggle_PB_LED */
+
+const byte LEDpin = 3;
+const byte SWITCHpin = 2;
+
+byte LEDstate = HIGH;
+byte SWITCHstate;
+byte lastSWITCHstate;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LEDpin, OUTPUT);
+pinMode(SWITCHpin, INPUT);
+digitalWrite(SWITCHpin, HIGH);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+SWITCHstate = digitalRead(SWITCHpin);
+if ((lastSWITCHstate == HIGH) && (SWITCHstate == LOW))
+{
+    LEDstate = !LEDstate;
+    digitalWrite(LEDpin, LEDstate);
+    delay(150);
+    
+    
+}
+lastSWITCHstate = SWITCHstate;
+}
